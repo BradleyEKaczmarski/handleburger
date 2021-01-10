7825a0836ea10d69cb6e67e4ea134637ac3544ca
BradleyEKaczmarski <69479265+BradleyEKaczmarski@users.noreply.github.com>
2021-01-10 12:43:05 -0500

    Handleburger

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rodeo Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fat Brad Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gyro Burger', FALSE);
